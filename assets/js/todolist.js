4b54e8f37c636e3c21213a38b86464d566ae5cec
Rafael Gioffi <rafaelgioffi@gmail.com>
2022-05-02 15:13:55 -0300

    Add files via upload

---
+++ assets/js/todolist.js
@@ -0,0 +1,31 @@
+const form = document.getElementById('task-form');
+const fulllist = document.getElementById('full-list');
+
+form.onsubmit = function (e) {
+    e.prevenDefault();
+    const inputField = document.getElementById('newTask');
+    addTask(inputField.value);
+    form.reset();
+};
+
+function addTask(descr) {
+    const taskContainer = document.createElement('div');
+    const inpTask = document.createElement('input');
+    const lblTask = document.createElement('label');
+    const descTaskNode = document.createTextNode(descr);
+    
+    inpTask.setAttribute('type', 'checkbox');
+    inpTask.setAttribute('name', descr);
+    inpTask.setAttribute('id', descr);
+
+    lblTask.setAttribute('for', descr);
+    lblTask.appendChild(descTaskNode);
+
+    taskContainer.classList.add('item');
+    taskContainer.appendChild(inpTask);
+    taskContainer.appendChild(lblTask);
+
+    fulllist.appendChild(taskContainer);
+    console.log(taskContainer);
+}
+
